69fffa8b9826a754897098cfe1ab58154452d542
joeangel <joe10330@gmail.com>
2016-05-16 12:13:53 +0800

    Fix instruction

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+cd
 git clone https://github.com/joeangel/rc-files.git
 
 # backup origin rc-files
-mkdir rc_backup
-cd rc-files
-find . -maxdepth 1 ! -path . ! -path ./.git ! -path ./setup.sh -exec mv ../{} ../rc_backup/ \;
+mkdir -p ~/rc_backup
+cd ~/rc-files
+find . -maxdepth 1 ! -path . ! -path ./.git ! -path ./setup.sh ! -path ./README.md -exec mv .{} ../rc_backup/. \;
 
-cd ..
+# move ~/rc-files/* to ~/
+cd
 find rc-files/ -maxdepth 1 ! -path rc-files/ ! -path rc-files/.git ! -path rc-files/setup.sh -exec ln -s {} . \;
