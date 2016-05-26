c90d3759bfca359d1b4c1577905953614e331d58
JoeAngel <joe10330@gmail.com>
2016-05-26 22:38:03 +0800

    Update setup script for Mac

--- setup.sh
+++ setup.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+exists() { [ ! -z `which "$1"` ]; }
+# or: exists() { type -t "$1" > /dev/null 2>&1; }
+
+# install homebrew if it is not exixts
+# http://brew.sh/
+if ! exists brew; then
+	echo 'Homebrew not found! install it!'
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# install useful tool
+brew install git ack tmux jq mtr
+# brew install gcc cmake
+# brew install jpegoptim
+# brew install ffmpeg
+# brew install dnsmasq mysql nvm
+
+# clone rc-files
 cd
 git clone https://github.com/joeangel/rc-files.git
 
@@ -10,4 +28,10 @@ find . -maxdepth 1 ! -path . ! -path ./.git ! -path ./setup.sh ! -path ./README.
 
 # move ~/rc-files/* to ~/
 cd
-find rc-files/ -maxdepth 1 ! -path rc-files/ ! -path rc-files/.git ! -path rc-files/.gitignore ! -path rc-files/setup.sh -exec ln -s {} . \;
+find rc-files -maxdepth 1 ! -path rc-files ! -path rc-files/.git ! -path rc-files/.gitignore ! -path rc-files/setup.sh -exec ln -s {} . \;
+
+source ~/.bashrc
+
+echo "1) Update ~/.bashrc for PS1 string"
+echo "2) Then, source ~/.bashrc"
+
