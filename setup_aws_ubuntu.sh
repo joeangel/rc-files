0ce2646a1e6df3d1d3913f41b5f49ddc9be16705
JoeAngel <joe10330@gmail.com>
2017-03-24 17:29:35 +0800

    Add setup_aws_ubuntu.sh

---
+++ setup_aws_ubuntu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# install useful tool
+sudo apt-get update && sudo apt-get install -y tmux exuberant-ctags ack-grep git jq tmux aptitude bash-completion
+
+# clone rc-files
+cd
+git clone https://github.com/joeangel/rc-files.git
+
+# backup origin rc-files
+mkdir -p ~/rc_backup
+cd ~/rc-files
+find . -maxdepth 1 ! -path . ! -path ./.git ! -path ./setup.sh ! -path ./README.md -exec mv .{} ../rc_backup/. \;
+
+# move ~/rc-files/* to ~/
+cd
+find rc-files/ -maxdepth 1 ! -path rc-files/ ! -path rc-files/.git ! -path rc-files/.gitignore ! -path rc-files/setup.sh ! -path rc-files/README.md ! -path rc-files/setup_ubuntu.sh ! -path rc-files/install_nvm_ubuntu.sh -exec ln -s {} . \;
+
+ +echo 'set-option -g prefix C-n' >> .tmux.conf
+ +rm -y .bashrc
+ +ln -s rc-files/.bashrc_pure .bashrc
+
+source ~/.bashrc
+
+# install Vundle for vim
+cd
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "1) Update ~/.bashrc for PS1 string"
+echo "2) Enter vi and type :BundleInstall"
+echo "3) Then, source ~/.bashrc"
+
